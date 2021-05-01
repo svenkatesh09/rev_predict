afe158b116229b04379d50d9009702cd014ce28d
svenkatesh09 <69424932+svenkatesh09@users.noreply.github.com>
2021-05-01 17:27:31 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+
+dataset = pd.read_csv('revenue.csv')
+
+dataset['rate'].fillna(0, inplace=True)
+
+dataset['sales_in_first_quarter'].fillna(dataset['sales_in_first_quarter'].mean(), inplace=True)
+
+X = dataset.iloc[:, :3]
+
+def convert_to_int(word):
+    word_dict = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8,
+                'nine':9, 'ten':10, 'eleven':11, 'twelve':12, 'zero':0, 0: 0}
+    return word_dict[word]
+
+X['rate'] = X['rate'].apply(lambda x : convert_to_int(x))
+
+y = dataset.iloc[:, -1]
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+
+regressor.fit(X, y)
+
+pickle.dump(regressor, open('model.pkl','wb'))
+
+model = pickle.load(open('model.pkl','rb'))
+print(model.predict([[4, 3328457.6, 2598044.8]]))
